ac207561f7047995a7bf3412ed5b58001aaf4fcd
Turistu <128076160+turistu@users.noreply.github.com>
2023-03-18 19:34:51 +0200

    stupid trick with an iframe in order to have the save password thing work in chrome too

--- page.js
+++ page.js
@@ -1,4 +1,4 @@
-function qs(s){ return document.querySelector(s) }
+function qs(s){ let e = document.querySelector(s); return e ? e : {} }
 function generate_code(e){
 	totp(qs('#key').value).then(c => {
 		qs('#code').textContent = c;
@@ -6,13 +6,11 @@ function generate_code(e){
 	}).catch(error => qs('#code').textContent = error);
 	e.preventDefault()
 }
-qs('#form').onsubmit = generate_code;
+qs('#generate').onclick = generate_code;
 qs('#key').oninput = function(e){
 	if(this != document.activeElement) return generate_code(e)
 }
-if(s = qs('#show')){
-	s.checked = false;
-	s.onchange = function(e){
-		qs('#key').type = this.checked ? 'text': 'password'
-	}
+qs('#show').checked = false;
+qs('#show').onchange = function(e){
+	qs('#key').type = this.checked ? 'text': 'password'
 }
