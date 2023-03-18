0b45df8592fd3aa5cca2e5251b9de1b95d7ce6ed
Turistu <128076160+turistu@users.noreply.github.com>
2023-03-18 22:14:13 +0200

    better error message

--- extra.js
+++ extra.js
@@ -2,8 +2,11 @@ function unbase32x(s){
 	let p = 0, t = s?.toLowerCase().match(/=+\s*$|\S/g)?.map(c => {
 		const i = "abcdefghijklmnopqrstuvwxyz234567".indexOf(c);
 		if(i >= 0) return i.fmt(2, 5);
-		if(p = c.match(/^(=|={3,4}|={6})\s*$/)?.[1].length) return '';
-		throw `bad char '${c}' in key`;
+		if(p = c.match(/^(=+)\s*$/)?.[1].length){
+			if([1, 3, 4, 6].includes(p)) return '';
+			throw `bogus padding with ${p} =s`
+		}else
+			throw `bad char '${c}' in key`;
 	}).join("");
 	if(!t) throw `empty key`;
 	if(t.length + p * 5 & 7) throw `bad ${t.length} bits key length`;
