eed184d9b2970136ea0061916b0d01be76865afe
Turistu <128076160+turistu@users.noreply.github.com>
2023-03-18 20:49:54 +0200

    nicer invalid input handling

--- page.js
+++ page.js
@@ -3,12 +3,15 @@ function generate_code(e){
 	totp(qs('#key').value).then(c => {
 		qs('#code').textContent = c;
 		navigator.clipboard.writeText(c);
-	}).catch(error => qs('#code').textContent = error);
-	e.preventDefault()
+	}).catch(error => {
+		qs('#error').textContent = 'ERROR: ' + error;
+		qs('#key').setCustomValidity('failed');
+	})
 }
 qs('#generate').onclick = generate_code;
 qs('#key').oninput = function(e){
 	if(this != document.activeElement) return generate_code(e)
+	else this.setCustomValidity('')
 }
 qs('#show').checked = false;
 qs('#show').onchange = function(e){
