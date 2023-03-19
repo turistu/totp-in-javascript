70b91503c9f2f9246801d34c08e9b5b73203cf8e
Turistu <128076160+turistu@users.noreply.github.com>
2023-03-19 10:08:36 +0200

    that was also matching =s in the middle of the key

--- extra.js
+++ extra.js
@@ -1,14 +1,12 @@
 function unbase32x(s){
-	let p = 0, t = s?.toLowerCase().match(/=+\s*$|\S/g)?.map(c => {
+	let p = 0, t = s?.toLowerCase().match(/=.*|\S/gs)?.map(c => {
 		const i = "abcdefghijklmnopqrstuvwxyz234567".indexOf(c);
 		if(i >= 0) return i.fmt(2, 5);
-		if(p = c.match(/^(=+)\s*$/)?.[1].length){
-			if([1, 3, 4, 6].includes(p)) return '';
-			throw `bogus padding with ${p} =s`
-		}else
-			throw `bad char '${c}' in key`;
+		if(/[^=\s]/.test(c)) throw `'${c}' not allowed in key`;
+		p = c.match(/=/g).length; return '';
 	}).join("");
 	if(!t) throw `empty key`;
+	if(p > 6 || p == 2 || p == 5) throw `bogus padding with ${p} '='s`;
 	if(t.length + p * 5 & 7) throw `bad ${t.length} bits key length`;
 	return new Uint8Array(t.match(/.{8}/g).map(d => parseInt(d, 2)));
 }
