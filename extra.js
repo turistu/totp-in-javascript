7d8c0100ee41677faabb6c24944c0eef1757c31b
Turistu <128076160+turistu@users.noreply.github.com>
2023-03-20 09:55:48 +0200

    better handling of invalid inputs in unbase32x

--- extra.js
+++ extra.js
@@ -1,12 +1,16 @@
 function unbase32x(s){
 	let p = 0, t = s?.toLowerCase().match(/=.*|\S/gs)?.map(c => {
-		const i = "abcdefghijklmnopqrstuvwxyz234567".indexOf(c);
+		const i = 'abcdefghijklmnopqrstuvwxyz234567'.indexOf(c);
 		if(i >= 0) return i.fmt(2, 5);
-		if(/[^=\s]/.test(c)) throw `'${c}' not allowed in key`;
+		if(/[^=\s]/.test(c)) throw `'${c}' not allowed in base32`;
 		p = c.match(/=/g).length; return '';
-	}).join("");
-	if(!t) throw `empty key`;
-	if(p > 6 || p == 2 || p == 5) throw `bogus padding with ${p} '='s`;
-	if(t.length + p * 5 & 7) throw `bad ${t.length} bits key length`;
+	}).join('');
+	if(!t) throw 'empty key';
+	if(p){
+		if(p > 6 || t.length + p * 5 & 7) throw 'bogus padding of key';
+		if((t.length & 7) > 4 || t.substring(t.length & ~7) - 0)
+			throw 'overlong base32 encoding of key';
+	}else if(t.length & 7)
+		throw `bad ${t.length} bits key length`;
 	return new Uint8Array(t.match(/.{8}/g).map(d => parseInt(d, 2)));
 }
