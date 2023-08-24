67c17ca587d5020c077da97e946a13d61bc398a9
Turistu <128076160+turistu@users.noreply.github.com>
2023-08-24 09:52:34 +0300

    improve selection + clipboard copy
    give some feedback on whether copy to clipboard has succeeded
    
    when pressing the "... copy to clipboard" button also select the code
    with getSelection().selectAllChildren(code_element), which on X11 has
    the effect of making its contents pastable with middle-click or
    left-button + paste.

--- page.js
+++ page.js
@@ -1,23 +1,28 @@
 function qs(s){ return document.querySelector(s) || {} }
-function generate_code(){
+function generate(event){
 	totp(qs('#key').value).then(c => {
-		qs('#code').textContent = c;
-		navigator.clipboard.writeText(c);
+		qs('#code').textContent = c; copy(event);
 	}).catch(error => {
 		qs('#error').textContent = 'ERROR: ' + error;
 		qs('#key').setCustomValidity('failed');
 	})
 }
-qs('#generate').onclick = generate_code;
+function copy(event){
+	const code = qs('#code'), sel = getSelection();
+	navigator.clipboard.writeText(code.textContent).then(() => {
+		code.title = 'copied!'; sel.selectAllChildren(code);
+	}).catch(error => {
+		code.title = event ? 'copy failed' : 'click to copy';
+		if(sel.containsNode(code)) sel.removeAllRanges();
+	})
+}
+qs('#generate').onclick = generate;
 qs('#key').oninput = function(){
-	this.setCustomValidity('');
-	qs('#error').innerHTML = '';
-	if(this !== document.activeElement) generate_code();
+	this.setCustomValidity(''); qs('#error').innerHTML = '';
+	if(this !== document.activeElement) generate();
 }
 qs('#show').checked = false;
 qs('#show').onchange = function(){
 	qs('#key').type = this.checked ? 'text': 'password';
 }
-qs('#code').onclick = function(){
-	navigator.clipboard.writeText(this.textContent);
-}
+qs('#code').onclick = copy;
