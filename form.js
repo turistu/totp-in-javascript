260d1e98ae7d59649bdcd9e1f5610548e9f319ae
Turistu <128076160+turistu@users.noreply.github.com>
2023-09-08 23:21:18 +0300

    Use <output> for outputs; makes for better semantics
    Also cut down a bit on the css-for-functionality abuse
    
    Revert to using a simple checkbox instead of the clickable button inside
    the input

--- form.js
+++ form.js
@@ -1,29 +1,25 @@
 function qs(s){ return document.querySelector(s) }
+function set_error(s){ qs('#key').setCustomValidity(s); qs('#error').value = s }
 function generate(){
 	totp(qs('#key').value).then(c => {
-		qs('#code').textContent = c; copy();
-	}).catch(error => {
-		qs('#error').textContent = 'ERROR: ' + error;
-		qs('#key').setCustomValidity('failed');
-	})
+		qs('#code').value = c; copy('click to copy');
+	}).catch(set_error);
 }
-function copy(e){
-	navigator.clipboard.writeText(qs('#code').textContent).then(() => {
+function copy(emsg){
+	navigator.clipboard.writeText(qs('#code').value).then(() => {
 		qs('#code').title = 'copied!';
 		if(navigator.userAgent.includes("(X11;"))
 			getSelection().selectAllChildren(qs('#code'));
-	}).catch(error => {
-		qs('#code').title = e ? 'copy failed' : 'click to copy';
-	})
+	}).catch(e => qs('#code').title = emsg);
 }
 qs('#generate').onclick = generate;
 qs('#key').oninput = function(){
-	this.setCustomValidity(''); qs('#error').innerHTML = '';
+	set_error('');
 	if(this !== document.activeElement) generate();
 }
-qs('#show').onclick = function(e){
-	qs('#key').type = qs('#key').type === 'text' ? 'password' : 'text';
-	e.preventDefault();
+qs('#show').checked = false;
+qs('#show').onchange = function(){
+	qs('#key').type = this.checked ? 'text': 'password';
 }
-qs('#code').onclick = copy;
+qs('#code').onclick = e => copy('copy failed');
 qs('form').className = document.location.hash.substr(1);
