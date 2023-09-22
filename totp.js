4f16a33c846d7b25a094fce22042867e57561b1d
Turistu <128076160+turistu@users.noreply.github.com>
2023-09-22 04:22:56 +0300

    get rid of "const" cargo cult that sucks; no functional change

--- totp.js
+++ totp.js
@@ -2,13 +2,13 @@ async function totp(key, secs = 30, digits = 6){
 	return hotp(unbase32(key), pack64bu(Date.now() / 1000 / secs), digits);
 }
 async function hotp(key, counter, digits){
-	const y = window.crypto.subtle;
+	let y = window.crypto.subtle;
 	if(!y) throw 'no window.crypto.subtle object available';
-	const k = await y.importKey('raw', key, {name: 'HMAC', hash: 'SHA-1'}, false, ['sign']);
+	let k = await y.importKey('raw', key, {name: 'HMAC', hash: 'SHA-1'}, false, ['sign']);
 	return hotp_truncate(await y.sign('HMAC', k, counter), digits);
 }
 function hotp_truncate(buf, digits){
-	const a = new Uint8Array(buf), i = a[19] & 0xf;
+	let a = new Uint8Array(buf), i = a[19] & 0xf;
 	return fmt(10, digits, ((a[i]&0x7f)<<24 | a[i+1]<<16 | a[i+2]<<8 | a[i+3]) % 10**digits);
 }
 
@@ -16,8 +16,8 @@ function fmt(base, width, num){
 	return num.toString(base).padStart(width, '0')
 }
 function unbase32(s){
-	const t = (s.toLowerCase().match(/\S/g)||[]).map(c => {
-		const i = 'abcdefghijklmnopqrstuvwxyz234567'.indexOf(c);
+	let t = (s.toLowerCase().match(/\S/g)||[]).map(c => {
+		let i = 'abcdefghijklmnopqrstuvwxyz234567'.indexOf(c);
 		if(i < 0) throw `bad char '${c}' in key`;
 		return fmt(2, 5, i);
 	}).join('');
