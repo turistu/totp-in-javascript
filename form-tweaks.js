62c0821d4cb8b4748db50b6e39f5b72625f2f0ca
Turistu <128076160+turistu@users.noreply.github.com>
2023-09-26 02:26:44 +0300

    use the already existing #id named variables instead of querySelector(#id)
    for each element with "id=foo", the browser is creating a window property
    (= global variable) with the same name referring to that element.
    
    that's a standard feature, not an obscure hack:
    https://html.spec.whatwg.org/multipage/nav-history-apis.html#named-access-on-the-window-object
    
    use that feature, making the id's uppercase so they stand out and do not
    clash with existing function names or variables.

--- form-tweaks.js
+++ form-tweaks.js
@@ -1,16 +1,16 @@
 if(window.PasswordCredential){
 	// chrome, edge
-	qs('form').onsubmit = function(e){
+	FORM.onsubmit = function(e){
 		navigator.credentials.store(new PasswordCredential({
-			id: qs('#user').value, password: qs('#key').value
+			id: USER.value, password: KEY.value
 		}));
 		e.preventDefault();
 	}
 }else if(!/Gecko\/\d/.test(navigator.userAgent)){
 	// firefox doesn't need this kludge
-	qs('form').onsubmit = null;
+	FORM.onsubmit = null;
 	let iframe = document.createElement('iframe');
 	iframe.style.display = 'none';
-	qs('form').target = iframe.name = 'fram';
+	FORM.target = iframe.name = 'fram';
 	document.body.appendChild(iframe);
 }
--- form.js
+++ form.js
@@ -1,34 +1,24 @@
-function qs(s){ return document.querySelector(s) }
 async function generate(){
-	try {
-		qs('#code').value = await totp(qs('#key').value);
-		copy('click to copy');
-	}catch(e){
-		qs('#key').setCustomValidity(e);
-		qs('#error').value = 'ERROR: ' + e;
-	}
+	try { CODE.value = await totp(KEY.value); copy('click to copy') }
+	catch(e){ KEY.setCustomValidity(ERROR.value = 'ERROR: ' + e) }
 }
 async function copy(emsg){
 	try {
-		await navigator.clipboard.writeText(qs('#code').value);
+		await navigator.clipboard.writeText(CODE.value);
 		if(navigator.userAgent.includes("(X11;"))
-			getSelection().selectAllChildren(qs('#code'));
-		qs('#code').title = 'copied!';
-	}catch(e){
-		qs('#code').title = emsg;
-	}
+			getSelection().selectAllChildren(CODE);
+		CODE.title = 'copied!';
+	}catch(e){ CODE.title = emsg }
 }
-qs('#generate').onclick = generate;
-qs('#key').oninput = function(){
-	this.setCustomValidity('');
-	qs('#error').value = this.checkValidity() ?
-		'' : 'ERROR: only A..Z, 2..7 and spaces allowed';
-	if(this !== document.activeElement) generate();
+GENERATE.onclick = generate;
+KEY.oninput = function(){
+	KEY.setCustomValidity('');
+	ERROR.value = KEY.checkValidity() ?  '' :
+		'ERROR: only A..Z, 2..7 and spaces allowed';
+	if(KEY !== document.activeElement) generate();
 }
-qs('#show').checked = false;
-qs('#show').onchange = function(){
-	qs('#key').type = this.checked ? 'text': 'password';
-}
-qs('#code').onclick = e => copy('copy failed');
-qs('form').className = document.location.hash.substr(1);
-qs('form').onsubmit = e => e.preventDefault();
+SHOW.checked = false;
+SHOW.onchange = e => KEY.type = SHOW.checked ? 'text': 'password';
+CODE.onclick = e => copy('copy failed');
+FORM.onsubmit = e => e.preventDefault();
+FORM.className = document.location.hash.substr(1);
